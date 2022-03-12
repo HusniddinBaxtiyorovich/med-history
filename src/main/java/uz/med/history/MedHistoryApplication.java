de66dd1b262d163c729bcbd9f7395819b9ade185
Husniddin <chorshanbiyev9944@gmail.com>
2022-03-12 23:11:32 +0500

    Initial project

---
+++ src/main/java/uz/med/history/MedHistoryApplication.java
@@ -0,0 +1,13 @@
+package uz.med.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedHistoryApplication.class, args);
+    }
+
+}
